f2e80186eac28e78e643e39a7457a49c870f8bff
RedHaile <sredeath@gmail.com>
2024-02-12 05:20:42 +0200

    file setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import logo from './logo.svg';
 
-const App = () => {
+import Search from './components/search/Search';
+import BreweryList from './components/products/BreweryList';
+
+function App() {
+  return (
+  
+<div className='App'>
+<BreweryList />
+<BreweryList></BreweryList>
+<Search></Search>
+</div>
+  );
+}
+/* const App = () => {
   return (
     <div>App</div>
   )
-}
+} */
 
 export default App
---
+++ src/components/products/BreweryList.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function BreweryListt() {
+  return (
+    <div>BreweryList</div>
+  )
+}
+export default BreweryListt;
